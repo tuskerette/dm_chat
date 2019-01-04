efb616ee2f823c4d93b444069b36b24edccb8090
tuskerette <vgraffeo@everquote.com>
2019-01-04 16:35:47 -0500

    Code cleanup and add logged in as.

--- public/app.js
+++ public/app.js
@@ -1,67 +1,48 @@
-// $('#app').hide();
-// $('#auth-form').on('submit', function(e) {
-//     e.preventDefault();
-//     if($('#password').val() == window.password) {
-//         $('#auth-form').hide();
-//         $('#app').show();
-//     }
-// });
-
 new Vue({
     el: '#app',
 
     data: {
-        ws: null, // websocket
-        newMsg: '',
-        chatContent: '',
-        username: null,
-        joined: false
+      ws: null, // websocket
+      newMsg: '',
+      chatContent: '',
+      username: null,
+      joined: false
+      // onlineUsers: ''
     },
 
     created: function() {
-        var self = this;
-        this.ws = new WebSocket('wss://' + window.location.host + '/ws');
-        this.ws.addEventListener('message', function(e) {
-            var msg = JSON.parse(e.data);
-            self.chatContent += '<div class="chip">'
-                    + msg.username
-                + '</div>'
-                + emojione.toImage(msg.message) + '<br/>'; // Parse emojis
-
-            var element = document.getElementById('chat-messages');
-            element.scrollTop = element.scrollHeight; // Auto scroll to the bottom
-        });
+      var self = this;
+      this.ws = new WebSocket('wss://' + window.location.host + '/ws'); //remember to put back 'wss://' when deploying!!! use 'ws://' in development
+      this.ws.addEventListener('message', function(e) {
+        var msg = JSON.parse(e.data);
+        self.chatContent += '<div class="chip">'+ msg.username +'</div>'+ msg.message +'<br/>';
+        var element = document.getElementById('chat-messages');
+        element.scrollTop = element.scrollHeight;
+      });
     },
 
     methods: {
-        send: function () {
-            if (this.newMsg != '') {
-                this.ws.send(
-                    JSON.stringify({
-                        username: this.username,
-                        message: $('<p>').html(this.newMsg).text() // Strip out html
-                    }
-                ));
-                this.newMsg = ''; // Reset newMsg
+      send: function () {
+        if (this.newMsg != '') {
+          this.ws.send(
+            JSON.stringify({
+              username: this.username,
+              message: $('<p>').html(this.newMsg).text()
             }
-        },
-
-        join: function () {
-            if (!this.username) {
-                Materialize.toast('You must choose a username', 2000);
-                return
-            }
-            $('.brand-logo').append(' ::: Logged in as: '+this.username);
-            this.joined = true;
-        },
-
-        exit: function () {
-            this.joined = false;
-        },
-
-        enter_password: function () {
-            this.password
+          ));
+          this.newMsg = '';
         }
-
+      },
+      join: function () {
+        if (!this.username) {
+          Materialize.toast('You must choose a username', 2000);
+          return
+        }
+        document.getElementById('logged-in-as').innerHTML = 'Logged in as: '+this.username;
+        this.joined = true;
+      },
+      exit: function () {
+        this.joined = false;
+      }
     }
 });
--- readme.md
+++ readme.md
@@ -6,3 +6,9 @@ Chat made with go and vue.js
 cd ./src
 go run main.go
 ```
+
+
+Next thing to be added:
+- Authentication either in go or in vue
+- Who's online
+- General cleanup/refactor
